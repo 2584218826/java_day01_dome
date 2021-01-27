87f1c2304cc3006c09e3034d3aea815453e53eb3
Mr.xu <2584218826@qq.com>
2021-01-27 18:52:33 +0800

    1、

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String name = "徐江涛";
+        long money= 5_000_000_0;
+
+        System.out.println(name+"的钱有"+money);
+    }
+}
---
+++ src/com/company/test.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.UUID;
+
+/**
+ * 内容：
+ *
+ * @author
+ * @date 2020/12/23-16:17
+ */
+public class test {
+	public static void main(String[] args) {
+		String token = UUID.randomUUID().toString();
+		System.out.println(token);
+	}
+}
